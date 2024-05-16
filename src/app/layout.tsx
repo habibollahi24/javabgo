b10298cddfe78fb8ab4d0809d65c2572492a863f
mohammad <habibollahi24@gmail.com>
2024-05-16 13:47:27 +0330

    change navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +17,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ul className="flex gap-8 justify-center py-4">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/privacy-policy">Privacy Policy</Link>
+          </li>
+          <li>
+            <Link href="/term">Term</Link>
+          </li>
+        </ul>
+        {children}
+      </body>
     </html>
   );
 }
